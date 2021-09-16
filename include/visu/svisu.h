d1fd4b7267cf14a60c19011a70cfbe115427ed74
René Horstmann <r.horsty@gmail.com>
2021-09-16 08:16:24 +0200

    Update svisu.h

--- include/visu/svisu.h
+++ include/visu/svisu.h
@@ -15,7 +15,7 @@
  *     SVisu *sv = svisu_new(true, 100);
  *
  *     // load a sphere into SVisu at x=100, y=100, z=0 with a radius of 50 in yellow color [mm]
- *     svisu_sphere(sv, (vec3) {{100, 100, 0}}, 50, SV_YELLOW.rgb);
+ *     svisu_sphere(sv, (vec3) {{100, 100, 0}}, 50, SV_YELLOW);
  *
  *     // show up SVisu window (gtk window)
  *     svisu_show(sv);
